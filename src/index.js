7b8213e0b7f99d8c13f8300996c7a81b97a16ddf
s.dudyuk.o <s.dudyuk.o@gmail.com>
2023-10-01 22:44:00 +0300

    add test

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 /*Task 1 */
 function logArguments(fn) {
+    return function () {
+        for (let i = 0; i < arguments.length; i++) {
+            console.log(`argument ${i}: ${arguments[i]}`);
+        }
 
-  return function() {
-
-      for (let i=0; i<arguments.length; i++){
-        console.log(`argument ${i}: ${arguments[i]}`);
-      }
-
-      return fn.apply(this, arguments);
-    }
-
+        return fn.apply(this, arguments);
+    };
 }
 
-/*Test: */
-const testFunction = (a,b,c) => {
-  console.log(a+b+c);
-}
-logArguments(testFunction)(1,2,3,4);
-
+/*Test:*/
+const testFunction = (a, b, c) => {
+    console.log(a + b + c);
+};
+logArguments(testFunction)(1, 2, 3, 4);
